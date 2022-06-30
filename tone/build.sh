ec895b45708ffe2f13c9ce36d7eb168dd1377fbd
sandreas <2050604+sandreas@users.noreply.github.com>
2022-06-30 07:02:17 +0200

    Fix microsoft issue on PublishReadyToRun

--- tone/build.sh
+++ tone/build.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+OBJ_DIR="$(dirname $0)/obj"
+R2R_DIR="$(find "$OBJ_DIR" -type d -name 'R2R')" #-exec rm -r {} \;
+
+if [ -d "$R2R_DIR" ]; then
+  # fix for https://github.com/dotnet/sdk/issues/21072
+  echo "fixing issue by deleting R2R: $R2R_DIR"
+  rm -r "$R2R_DIR"
+fi 
+
 dotnet publish -r linux-x64 --self-contained -c Release -o ./dist tone.csproj
