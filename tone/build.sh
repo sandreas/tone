b6b4747373c317485d816ff42996060a645bdc00
sandreas <2050604+sandreas@users.noreply.github.com>
2022-07-01 08:03:24 +0200

    Added first try of automated docker multiarch image generation

--- tone/build.sh
+++ tone/build.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-OBJ_DIR="$(dirname $0)/obj"
-R2R_DIR="$(find "$OBJ_DIR" -type d -name 'R2R')" #-exec rm -r {} \;
+OBJ_DIR="${PWD}/obj"
 
-if [ -d "$R2R_DIR" ]; then
-  # fix for https://github.com/dotnet/sdk/issues/21072
-  echo "fixing issue by deleting R2R: $R2R_DIR"
-  rm -r "$R2R_DIR"
-fi 
+# fix for https://github.com/dotnet/sdk/issues/21072
+find "$OBJ_DIR" -type d -name 'R2R' -exec rm -r "{}" \;
 
 dotnet publish -r linux-x64 --self-contained -c Release -o ./dist tone.csproj
