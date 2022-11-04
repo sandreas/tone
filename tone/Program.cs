275adb6bd77d273eb9316d98f79d2fe0738bcb4c
sandreas <2050604+sandreas@users.noreply.github.com>
2022-11-05 00:35:15 +0100

    Bumped version and added new release-notes-v0.1.3.md

--- tone/Program.cs
+++ tone/Program.cs
@@ -200,7 +200,7 @@ try
         config.UseStrictParsing();
         config.CaseSensitivity(CaseSensitivity.None);
         config.SetApplicationName("tone");
-        config.SetApplicationVersion("0.1.2");
+        config.SetApplicationVersion("0.1.3");
         config.ValidateExamples();
         config.AddCommand<DumpCommand>("dump")
             .WithDescription("dump metadata for files and directories (directories are traversed recursively)")
