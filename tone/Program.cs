885b804a1385e01106d407dda8c5f04c22ad6464
sandreas <2050604+sandreas@users.noreply.github.com>
2024-06-03 12:07:13 +0200

    updated version number to 0.1.7

--- tone/Program.cs
+++ tone/Program.cs
@@ -223,7 +223,7 @@ try
         config.UseStrictParsing();
         config.CaseSensitivity(CaseSensitivity.None);
         config.SetApplicationName("tone");
-        config.SetApplicationVersion("0.1.6");
+        config.SetApplicationVersion("0.1.7");
         config.ValidateExamples();
         config.AddCommand<DumpCommand>("dump")
             .WithDescription("dump metadata for files and directories (directories are traversed recursively)")
